dc7862bd7780109ea9e5b62821b1fa6088a845dc
hallan-n <hallanneves01@gmail.com>
2026-02-08 08:50:33 -0300

    Refactor: Improve file attachment process and message formatting in WhatsApp automation

--- app/whatsapp.py
+++ app/whatsapp.py
@@ -14,26 +14,55 @@ class Whatsapp:
             page = context.pages[0]
             await page.goto("https://web.whatsapp.com/")
 
-            await page.wait_for_timeout(10000)
-            lista_conversas = await page.query_selector(
-                'div[aria-label="Lista de conversas"]'
-            )
+            await page.wait_for_selector('div:has-text("Protegida com a criptografia de ponta a ponta")', state="detached")
+
+            await page.wait_for_timeout(2000)
+
+            lista_conversas = await page.query_selector('div[aria-label="Lista de conversas"]')
 
             await page.wait_for_timeout(1000)
-            conversa_alan = await lista_conversas.query_selector(
-                'div[role="row"]:has(span:text("Alan"))'
-            )
+
+            conversa_alan = await lista_conversas.query_selector('div[role="row"]:has(span:text("Alan"))')
+
             await page.wait_for_timeout(1000)
+
             await conversa_alan.click()
+
             await page.wait_for_timeout(1000)
 
-            await page.add_init_script(
-                f"""
-                    () => {{
-                        const html = "teste";
-                        let chat = document.querySelector('div[aria-placeholder="Digite uma mensagem"]')
-                        
-                    }}
-                """
-            )
+            await page.click('button[aria-label="Anexar"]')
 
+            await page.wait_for_timeout(1000)
+
+            async with page.expect_file_chooser() as fc:
+                await page.get_by_label("Fotos e vídeos").click()
+
+            file_chooser = await fc.value
+            await file_chooser.set_files("/home/neves/Documentos/promo-bot/1.png")
+
+
+            box = page.locator('div[contenteditable="true"][aria-label="Digite uma mensagem"]')
+
+            await page.wait_for_timeout(1000)
+
+            await box.click()
+
+            await page.wait_for_timeout(1000)
+
+            await box.fill(
+"""🧜🏻‍♀️ Notebook Gamer Acer Nitro V15 Core I5 512gb 8gb Linux+ubook
+
+~de R$ 4.699~
+por R$ 3.819,00 😱😱
+
+💳 ou 10x de R$ 381,90
+🎟️ Aplique o cupom de R$300 OFF abaixo do preço
+
+https://mercadolivre.com/sec/1juisjF""")
+
+
+            
+            await page.wait_for_timeout(1000)
+
+            await page.keyboard.press("Enter")
+            breakpoint()
